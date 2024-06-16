7f7c4ad8e0538651919cdfd752f6d374095aac58
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-16 13:47:36 +0530

    data array

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -1,1 +1,18 @@
 console.log("monogdb");
+
+const blogSchema ={
+    title: String, // String is shorthand for {type: String}
+    author: String,
+    body: String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+      votes: Number,
+      favs: Number
+    }
+  };
+
+
+
+  console.log(blogSchema.comments[0].body);
